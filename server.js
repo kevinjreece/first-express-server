e26674c46cce04bfcc44cb2167bbc5cd0ca6db3f
Kevin J Reece <kevinjreece@gmail.com>
2015-03-19 12:52:02 -0600

    added https

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var options = {
 	host: '127.0.0.1',
 	key: fs.readFileSync('ssl/server.key'),
 	cert: fs.readFileSync('ssl/server.crt')
-}
-app.use('/', express.static('./html', {maxAge: 60*60*1000}));
-app.listen(80);
+};
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+app.get('/', function(req, res) {
+	res.send("Get Index");
+});

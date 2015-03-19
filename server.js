19cd4d1ab66649b8049c57792a918c7c8d7f3717
Kevin J Reece <kevinjreece@gmail.com>
2015-03-19 12:43:14 -0600

    Step 1 of hints

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 var app = express();
+var options = {
+	host: '127.0.0.1',
+	key: fs.readFileSync('ssl/server.key'),
+	cert: fs.readFileSync('ssl/server.cert')
+}
 app.use('/', express.static('./html', {maxAge: 60*60*1000}));
 app.listen(80);

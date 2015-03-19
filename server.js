1af7d779d1692b463e49410baa4cb2aeb6439f8a
Kevin J Reece <kevinjreece@gmail.com>
2015-03-19 12:44:59 -0600

    .cert to .crt

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var options = {
 	host: '127.0.0.1',
 	key: fs.readFileSync('ssl/server.key'),
-	cert: fs.readFileSync('ssl/server.cert')
+	cert: fs.readFileSync('ssl/server.crt')
 }
 app.use('/', express.static('./html', {maxAge: 60*60*1000}));
 app.listen(80);

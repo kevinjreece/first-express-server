f8cb2d365141583bd1c402237a9164cd00e10ecd
Kevin J Reece <kevinjreece@gmail.com>
2015-03-19 21:36:01 -0600

    Implemented step 4

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var https = require('https');
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser());
 var options = {
 	host: '127.0.0.1',
 	key: fs.readFileSync('ssl/server.key'),
@@ -16,3 +18,12 @@ app.get('/getcity', function(req, res) {
 	console.log("In getcity route");
 	res.json([{city: "Price"},{city: "Provo"}]);
 });
+app.get('/comment', function(req, res) {
+	console.log("In comment route");
+});
+app.post('/comment', function(req, res) {
+	console.log("In POST comment route");
+	console.log(req.body);
+	res.status(200);
+	res.end();
+})
